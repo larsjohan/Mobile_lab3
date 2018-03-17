ba33c8e74c183c8f4596e6f3ae365c34a8c3653b
Lars Johan <larsjny@stud.ntnu.no>
2018-03-17 13:56:46 +0100

    Initial Commit

---
+++ app/src/main/java/no/ntnu/stud/larsjny/lab3/BallGame.java
@@ -0,0 +1,13 @@
+package no.ntnu.stud.larsjny.lab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BallGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ball_game);
+    }
+}
